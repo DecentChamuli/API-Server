41a5301b0c9c3cc5554133a1926529d00436dcd1
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2021-11-02 12:58:41 +0500

    Changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const rateLimit = require('express-rate-limit')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 require('dotenv').config()
 
 const app = express()
@@ -22,7 +22,16 @@ app.set('trust proxy', 1)
 // app.use('/api', require('./routes'))
 app.use('/', require('./routes'))
 
-app.use(cors())
+app.use(
+    cors({
+        origin: ["http://localhost", "127.0.0.1"],
+        methods: ["GET"]
+    })
+)
 
-app.listen(PORT, ()=> console.log(`Server running on port http://localhost:${PORT}`))
+app.listen(PORT, ()=> {
+    if(process.env.NODE_ENV !== 'production'){
+        console.log(`Server running on http://localhost:${PORT}`)
+    }
+})
 

4a5c79e3c1bf5b79ae21186bac014fd910b2f8c6
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2021-11-10 21:21:00 +0500

    YT Video Download

--- routes/weather.js
+++ routes/weather.js
@@ -4,12 +4,8 @@ const needle = require('needle')
 const url = require('url')
 const apicache = require('apicache')
 
-// Env Variables
-const WEATHER_API_BASE_URL = process.env.WEATHER_API_BASE_URL
-const WEATHER_API_KEY_NAME = process.env.WEATHER_API_KEY_NAME
+// Environment Variables
 const WEATHER_API_KEY_VALUE = process.env.WEATHER_API_KEY_VALUE
-const WEATHER_API_UNIT_NAME = process.env.WEATHER_API_UNIT_NAME 
-const WEATHER_API_UNIT_VALUE = process.env.WEATHER_API_UNIT_VALUE
 
 // Initialize Cache
 let cache = apicache.middleware
@@ -17,16 +13,18 @@ let cache = apicache.middleware
 router.get('/', cache('1 minutes'), async (req,res) => {
     try {    
         const params = new URLSearchParams({
-            [WEATHER_API_KEY_NAME]: WEATHER_API_KEY_VALUE,
-            [WEATHER_API_UNIT_NAME]: WEATHER_API_UNIT_VALUE,
-            q: 'windsor',
+            appid: WEATHER_API_KEY_VALUE,
+            units: 'metric',
+            q: 'karachi',
             ...url.parse(req.url, true).query
         })
 
-        const apiRes = await needle('get', `${WEATHER_API_BASE_URL}?${params}`)
+        const base_url = 'https://api.openweathermap.org/data/2.5/weather'
+
+        const apiRes = await needle('get', `${base_url}?${params}`)
         const data = apiRes.body
 
-        res.status(200).json({data})        
+        res.status(200).json({data})
         
     } catch (error) {
         res.status(500).json({error})  
--- routes/youtube.js
+++ routes/youtube.js
@@ -1,8 +1,44 @@
 const express = require('express')
+// const fs = require('fs')
+const ytdl = require('ytdl-core')
 const router = express.Router()
 
+const convertUrl = (url) => {
+    let newUrlArray;
+    if(url.includes("youtu.be")){
+        newUrlArray = url.split("youtu.be/")
+        return `https://www.youtube.com/watch?v=${newUrlArray[1]}`
+    }
+    else if(url.includes("www.youtube.com/shorts/")){
+        newUrlArray = url.split("www.youtube.com/shorts/")
+        return `https://www.youtube.com/watch?v=${newUrlArray[1]}`
+    }
+    else if(url.includes("www.youtube.com/watch?v")){
+        return url
+    }
+    else{
+        return 'https://www.youtube.com/watch?v=p8NrTxybc6c'
+    }
+}
+
+let full ='https://www.youtube.com/watch?v=p8NrTxybc6c'
+let small = 'https://youtu.be/p8NrTxybc6c'
+let short = 'https://www.youtube.com/shorts/NurNN_g1rZM'
+
 router.get('/', async (req,res) => {
-    res.send('hello')
+    const {itag,title,type} = req.query
+	const video = convertUrl(req.query.video)
+
+	if(type === "mp4"){
+		res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
+		res.header('Content-Type', 'video/mp4');
+	}else if(type === "mp3"){
+		res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
+		res.header('Content-Type', 'audio/mp3');
+	}
+
+	ytdl(video, { filter: format => format.itag === parseInt(itag) }).pipe(res);
+    
 })
 
 module.exports = router

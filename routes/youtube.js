25e4d0dd2f8c5185aec6c6f38b713803be28a4ac
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2022-04-27 02:26:47 +0500

    Merging thorugh Route Finally Works using Merge1

--- routes/youtube.js
+++ routes/youtube.js
@@ -130,127 +130,59 @@ router.get('/merge', async (req, res)=>{
 })
 
 router.get('/merge1', async (req, res)=>{
-    const itag = req.query.itag
+    // const quality = req.query.quality
 	const video = convertUrl(req.query.video)
 
 	let info = await ytdl.getInfo(video)
 	let title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "")
 
-	/* 
-		248 ---> 1080p webm
-		244 ---> 480p webm
-		242 ---> 240p webm
-		
-		137 ---> 1080p mp4
-		135 ---> 480p mp4
-		133 ---> 240p mp4
-		
-		140 ---> 128k m4a
-	*/
-
 	res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
 	res.header('Content-Type', 'video/mp4')
 
-	try {
-		vid = ytdl(video, {filter: format => format.itag === parseInt(itag)})
-		aud = ytdl(video, {filter: format => format.itag === '140'})
+	vid = ytdl(video, {filter: format => format.qualityLabel === '480p'})
+	aud = ytdl(video, { quality: 'lowestaudio' })
 
-		// ytdl(video, {filter: format => format.itag === parseInt(itag)}).pipe(res)
-		// ytdl(video, {filter: format => format.itag === '140'}).pipe(res)
+	let ffmpegLogs = ''
 
-		const ref = 'https://www.youtube.com/watch?v=aqz-KE-bpKQ';
-		const tracker = {
-			start: Date.now(),
-			audio: { downloaded: 0, total: Infinity },
-			video: { downloaded: 0, total: Infinity },
-			merged: { frame: 0, speed: '0x', fps: 0 },
-		};
-
-		// Get audio and video streams
-		const audio = ytdl(ref, { quality: 'highestaudio' })
-		.on('progress', (_, downloaded, total) => {
-			tracker.audio = { downloaded, total };
-		});
-		const video = ytdl(ref, { quality: 'highestvideo' })
-		.on('progress', (_, downloaded, total) => {
-			tracker.video = { downloaded, total };
-		});
-
-		// Prepare the progress bar
-		let progressbarHandle = null;
-		const progressbarInterval = 1000;
-		const showProgress = () => {
-			readline.cursorTo(process.stdout, 0);
-			const toMB = i => (i / 1024 / 1024).toFixed(2);
-
-			process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
-			process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
-
-			process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
-			process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
-
-			process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
-			process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
-
-			process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
-			readline.moveCursor(process.stdout, 0, -3);
-		};
-
-		// Start the ffmpeg child process
-		const ffmpegProcess = cp.spawn(ffmpeg, [
-
-			// Remove ffmpeg's console spamming
-			'-loglevel', '8', '-hide_banner',
-			// Redirect/Enable progress messages
-			'-progress', 'pipe:3',
-			// Set inputs
-			'-i', 'pipe:4',
-			'-i', 'pipe:5',
-			// Map audio & video from streams
-			'-map', '0:a',
-			'-map', '1:v',
-			// Keep encoding
-			'-c:v', 'copy',
-			// Define output file
-			'out.mkv'
-		], {
-		windowsHide: true,
+	const ffmpegProcess = cp.spawn(ffmpeg, [
+		'-i', `pipe:3`,
+		'-i', `pipe:4`,
+		'-map','0:v',
+		'-map','1:a',
+		'-c:v', 'copy',
+		'-c:a', 'libmp3lame',
+		'-crf','27',
+		'-preset','veryfast',
+		'-movflags','frag_keyframe+empty_moov',
+		'-f','mp4',
+		'-loglevel','error',
+		'-'
+	], {
 		stdio: [
-			/* Standard: stdin, stdout, stderr */
-			'inherit', 'inherit', 'inherit',
-			/* Custom: pipe:3, pipe:4, pipe:5 */
-			'pipe', 'pipe', 'pipe',
+		'pipe', 'pipe', 'pipe', 'pipe', 'pipe',
 		],
-		});
-		ffmpegProcess.on('close', () => {
-		console.log('done');
-		// Cleanup
-		process.stdout.write('\n\n\n\n');
-		clearInterval(progressbarHandle);
-		});
-
-		// Link streams
-		// FFmpeg creates the transformer streams and we just have to insert / read data
-		ffmpegProcess.stdio[3].on('data', chunk => {
-		// Start the progress bar
-		if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
-		// Parse the param=value list returned by ffmpeg
-		const lines = chunk.toString().trim().split('\n');
-		const args = {};
-		for (const l of lines) {
-			const [key, value] = l.split('=');
-			args[key.trim()] = value.trim();
+	});
+	
+	vid.pipe(ffmpegProcess.stdio[3]);
+	aud.pipe(ffmpegProcess.stdio[4]);
+	ffmpegProcess.stdio[1].pipe(res);
+	
+	ffmpegProcess.stdio[2].on(
+		'data',
+		(chunk)=>{ffmpegLogs += chunk.toString()}
+	)
+	
+	ffmpegProcess.on(
+		'exit',
+		(exitCode)=>{
+			if(exitCode === 1){
+				console.error(ffmpegLogs)
+			}
 		}
-		tracker.merged = args;
-		});
-		audio.pipe(ffmpegProcess.stdio[4]);
-		video.pipe(ffmpegProcess.stdio[5]);
-	}
-	catch(err) {
-		res.send(err)
-	}
+	)
 })
 
+// This is Route where all test are performed
 router.get('/try', async (req, res)=>{
 	const itag = req.query.itag
 	const video = convertUrl(req.query.video)
